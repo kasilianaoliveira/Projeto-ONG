1b1499ef11bb102ff80d7f89489c2962e7e0e2c5
kasilianaoliveira <kasilianaoliveira@gmail.com>
2021-08-17 00:17:42 -0300

    back, front e modelagem

---
+++ backend/src/index.js
@@ -0,0 +1,4 @@
+const app = require('./server')
+
+const port = 3333;
+app.listen(port, console.log('Server is running'));
---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes/routes');
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + "/src/pages/index.html");
+});
+
+app.use((request, response, next) =>{
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error)
+});
+
+app.use((error, request, response, next) =>{
+  response.status(error.status || 500);
+  response.json({ error: error.message})
+});
+
+module.exports = app;
+
+
